f194290365f211b1a1e346524a04dc9886129950
Syver <114483860+syver-syver@users.noreply.github.com>
2023-02-19 17:34:32 +0100

    remove old file

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ function init() {
     // Local storage explanation https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/stringify
     const shopping_list_from_storage = JSON.parse(localStorage.getItem('shopping_list')) || [];
 
-    shopping_list_from_storage.forEach((item) => {
+    function render_list_item(item) {
         const li_element = document.createElement('li');
         const li_checkbox = document.createElement('input');
         li_checkbox.type = 'checkbox';
@@ -43,60 +43,55 @@ function init() {
             localStorage.setItem('shopping_list', JSON.stringify(shopping_list_from_storage));
             li_element.remove();
         });
-        input_field.value = '';
-    });
+    }
 
+    shopping_list_from_storage.forEach(item => render_list_item(item));
     section_element.appendChild(ul_element);
 
-    submit_button.addEventListener('click', add_list_item);
+    submit_button.addEventListener('click', () => {
+        if (input_field.value == '') {
+            alert('Skriv noe i feltet først')
+        }
+        else {
+            const item = { value: input_field.value, checked: false };
+            shopping_list_from_storage.push(item);
+            localStorage.setItem('shopping_list', JSON.stringify(shopping_list_from_storage));
+            render_list_item(item);
+            input_field.value = '';
+        };
+    });
     remove_button.addEventListener('click', remove_all_list_items);
     input_field.addEventListener('keyup', (event) => {
         if (event.key === 'Enter') {
-            add_list_item()
-        }
+            if (input_field.value == '') {
+                alert('Skriv noe i feltet først')
+            }
+            else {
+                const item = { value: input_field.value, checked: false };
+                shopping_list_from_storage.push(item);
+                localStorage.setItem('shopping_list', JSON.stringify(shopping_list_from_storage));
+                render_list_item(item);
+                input_field.value = '';
+            };
+        };
     });
 
-    function add_list_item() {
-        const li_element = document.createElement('li');
-        const li_checkbox = document.createElement('input');
-        li_checkbox.type = 'checkbox';
-        li_checkbox.className = 'checkbox';
-        const li_span_element = document.createElement('span')
-        const li_delete_button = document.createElement('button');
-        li_delete_button.className = 'li_delete_button';
+    // Lenke til hvordan jeg kan ha javascript på bare pc https://stackoverflow.com/a/42044841
+    function remove_button_hover() {
+        if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+            remove_button.addEventListener('mouseover', () => {
+                remove_button.style.backgroundImage = "url('/assets/Søppel_icon_hover.svg')";
+            });
 
-        const item = { value: input_field.value, checked: false };
+            remove_button.addEventListener('mouseout', () => {
+                remove_button.style.backgroundImage = "url('/assets/Søppel_icon.svg')";
+            });
+        }
+        else {
+        };
+    };
 
-        li_span_element.innerText = item.value;
-        li_element.prepend(li_checkbox);
-        li_element.appendChild(li_span_element);
-        li_element.appendChild(li_delete_button);
-        ul_element.appendChild(li_element);
-        shopping_list_from_storage.push(item);
-
-        localStorage.setItem('shopping_list', JSON.stringify(shopping_list_from_storage));
-
-        li_checkbox.addEventListener('keyup', (event) => {
-            if (event.key === 'Enter') {
-                li_checkbox.checked = !li_checkbox.checked;
-            }
-            item.checked = event.target.checked;
-            localStorage.setItem('shopping_list', JSON.stringify(shopping_list_from_storage));
-        });
-
-        li_checkbox.addEventListener('change', (event) => {
-            item.checked = event.target.checked;
-            localStorage.setItem('shopping_list', JSON.stringify(shopping_list_from_storage));
-        });
-
-        li_delete_button.addEventListener('click', () => {
-            const index = shopping_list_from_storage.indexOf(item);
-            shopping_list_from_storage.splice(index, 1);
-            localStorage.setItem('shopping_list', JSON.stringify(shopping_list_from_storage));
-            li_element.remove();
-        });
-        input_field.value = '';
-    }
+    remove_button_hover();
 
     function remove_all_list_items() {
         while (ul_element.firstChild) {
@@ -107,5 +102,3 @@ function init() {
 };
 
 init();
-
-
